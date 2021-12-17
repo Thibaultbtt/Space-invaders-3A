aa9df842804e03e45b737bfd0056698cbc7f082e
Thibault <thib.berth@gmail.com>
2021-12-17 09:18:17 +0100

    ddd

--- space_invader.py
+++ space_invader.py
@@ -6,7 +6,6 @@ from random import randint as rd
 Application = tk.Tk()
 
 can_jeu=tk.Canvas(bg='brown', width=906, height=650)
-can_jeu.place(x=10,y=12)
 
 
 
@@ -16,7 +15,8 @@ can_jeu.place(x=10,y=12)
 
 Application.title("Space Invaders")
 Application.geometry("1400x900")
-Application.configure(bg="green")
+#Application.configure(background='green')
+Application['background'] = 'green'
 
 
 ##----- Programme principal -----##
