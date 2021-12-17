7b626eee6f4cc9c120634cb785332349fd310a9f
Thibault <thib.berth@gmail.com>
2021-12-17 09:09:40 +0100

    fenetre

---
+++ space_invader.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from random import randint as rd
+
+
+
+Application = tk.Tk()
+
+can_jeu=tk.Canvas(bg='brown', width=906, height=650)
+can_jeu.place(x=10,y=12)
+
+
+
+
+
+
+
+Application.title("Space Invaders")
+Application.geometry("1400x900")
+Application.configure(bg="green")
+
+
+##----- Programme principal -----##
+Application.mainloop()# Boucle d'attente des événements
