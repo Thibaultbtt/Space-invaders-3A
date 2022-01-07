91217cc935eb49e0bf57496336d807b3893f715b
Lauria <lauria.castanier@gmail.com>
2022-01-07 08:12:01 +0100

    modif vacs

--- space_invader.py
+++ space_invader.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from random import randint as rd
+import random as rd
 
 
 
@@ -28,52 +28,83 @@ can_jeu.config(highlightthickness=0)
 
 
 def droite (event):
-   if can_jeu.coords(papa)[0]<1050 :
-       can_jeu.move(papa, 20 , 0 )
+  if can_jeu.coords(papa)[0]<1050 :
+      can_jeu.move(papa, 20 , 0 )
 
 can_jeu.bind_all('<Right>', droite)
 
 def gauche (event):
-   if can_jeu.coords(papa)[0]>50 :
-       can_jeu.move(papa, -20 , 0 )
+  if can_jeu.coords(papa)[0]>50 :
+      can_jeu.move(papa, -20 , 0 )
 can_jeu.bind_all('<Left>', gauche)
 
 lutin=tk.PhotoImage(file ="lutin.png")
-elfe=can_jeu.create_image(500,100,image=lutin)
-can_jeu.config(highlightthickness=0)
+flocon=tk.PhotoImage(file ="flocon.png")
+x=6
 
-x=10
+def chute_flocon ():
+   if liste_elfes == [] :
+          return
+   lutin_random = rd.choice(liste_elfes)
+   abscisse = can_jeu.coords(lutin_random)[0]
+   ordonee = 70
+   neige = can_jeu.create_image(abscisse,ordonee,image=flocon)
+   deplacement_flocon(neige)
+   Application.after(1500,chute_flocon)
 
-def deplacement_lutin() :
-   global x
-   dx = can_jeu.coords(elfe)[0]
-   if dx < 1050 and dx > 50 :
-       can_jeu.move(elfe, x , 0 )
-   else :
-       x=-1*x
-       can_jeu.move(elfe, x , 0 )
-   Application.after(80,deplacement_lutin)
+def deplacement_flocon (neige) :
+   can_jeu.move(neige, 0 , 10 )
+   Application.after(70,lambda : deplacement_flocon(neige))
 
-deplacement_lutin()
+liste_elfes = []
+abscisse = 60
+ordonnee = 100
+for i in range(5) :
+    elfe = can_jeu.create_image(abscisse,ordonnee,image=lutin)
+    liste_elfes.append(elfe)
+    abscisse += 200
+
+chute_flocon ()
+
+def deplacement_lutin(elfe) :
+  global x
+  if liste_elfes == [] :
+      return
+  dx = can_jeu.coords(elfe)[0]
+  if dx < 1050 and dx > 50 :
+      can_jeu.move(elfe, x , 0 )
+  else :
+      x=-1*x
+      can_jeu.move(elfe, x , 0 )
+  Application.after(80,lambda : deplacement_lutin(elfe))
+
+for elfe in liste_elfes :
+   deplacement_lutin(elfe)
 
 cadeau=tk.PhotoImage(file ="cadeau.png")
 
 def tire(event):
-   abscisse = can_jeu.coords(papa)[0]
-   ordonee = 430
-   gift=can_jeu.create_image(abscisse,ordonee,image=cadeau)
-   deplacement_cadeau (gift)
+  abscisse = can_jeu.coords(papa)[0]
+  ordonee = 430
+  gift=can_jeu.create_image(abscisse,ordonee,image=cadeau)
+  deplacement_cadeau (gift)
 
 def deplacement_cadeau (gift) :
-   can_jeu.move(gift, 0 , -10 )
-   if can_jeu.coords(gift)[1] <= 25 :
-       can_jeu.delete(gift)
-       return
-   if can_jeu.coords(gift)[0] == can_jeu.coords(elfe)[0] and can_jeu.coords(gift)[1] == can_jeu.coords(elfe)[1] :
-       can_jeu.delete(gift)
-       can_jeu.delete(elfe)
-       return
-   Application.after(80,lambda : deplacement_cadeau(gift))
+  can_jeu.move(gift, 0 , -20 )
+  if can_jeu.coords(gift)[1] <= 25 :
+      can_jeu.delete(gift)
+      return
+  if liste_elfes == [] :
+      print('Gagné !!!!')
+      return
+  for elfe in liste_elfes :
+      coordonnees = can_jeu.coords(elfe)
+      if coordonnees[0] < can_jeu.coords(gift)[0] < coordonnees[0]+30 and coordonnees[1] < can_jeu.coords(gift)[1] < coordonnees[1]+30 :
+          liste_elfes.remove(elfe)
+          can_jeu.delete(gift)
+          can_jeu.delete(elfe)
+          return
+  Application.after(80,lambda : deplacement_cadeau(gift))
 
 can_jeu.bind_all('<space>', tire)
 
